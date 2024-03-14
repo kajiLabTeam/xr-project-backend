5e69eed7ebe42eb83fc7fdfa1fe728a994968cfe
Yusei Mizutani <yusei4520030401@gmail.com>
2024-03-15 01:18:32 +0900

    fix : サンプルデータを修正

--- docker/postgres/init/2-insert_sample.sql
+++ docker/postgres/init/2-insert_sample.sql
@@ -11,21 +11,3 @@ INSERT INTO users (id, name, mail, gender, age, height, weight, address, occupat
 VALUES
     ('01F8VYXK67BGC1F9RP1E4S9YTX', 'Alice', 'alice@example.com', 'Female', 25, 160.5, 55.0, '789 Elm St, Village', 'Engineer', '01F8VYXK67BGC1F9RP1E4S9YTV'),
     ('01F8VYXK67BGC1F9RP1E4S9YTZ', 'Bob', 'bob@example.com', 'Male', 30, 175.0, 70.0, '567 Pine St, Town', 'Teacher', '01F8VYXK67BGC1F9RP1E4S9YTW');
-
--- spotsテーブルにサンプルデータを手動で挿入
-INSERT INTO spots (id, name, floor, location_type)
-VALUES
-    ('01F8VYXK67BGC1F9RP1E4S9YUA', 'Room A', 1, 'indoor'),
-    ('01F8VYXK67BGC1F9RP1E4S9YUB', 'Garden', 0, 'outdoor');
-
--- objectsテーブルにサンプルデータを手動で挿入
-INSERT INTO objects (id, extension, user_id, spot_id)
-VALUES
-    ('01F8VYXK67BGC1F9RP1E4S9YUC', 'jpg', '01F8VYXK67BGC1F9RP1E4S9YTX', '01F8VYXK67BGC1F9RP1E4S9YUA'),
-    ('01F8VYXK67BGC1F9RP1E4S9YUD', 'png', '01F8VYXK67BGC1F9RP1E4S9YTZ', '01F8VYXK67BGC1F9RP1E4S9YUB');
-
--- object_browsing_logsテーブルにサンプルデータを手動で挿入
-INSERT INTO object_browsing_logs (id, reading_date, user_id, object_id)
-VALUES
-    ('01F8VYXK67BGC1F9RP1E4S9YUE', '2024-03-02T12:00:00Z', '01F8VYXK67BGC1F9RP1E4S9YTX', '01F8VYXK67BGC1F9RP1E4S9YUC'),
-    ('01F8VYXK67BGC1F9RP1E4S9YUF', '2024-03-03T10:30:00Z', '01F8VYXK67BGC1F9RP1E4S9YTZ', '01F8VYXK67BGC1F9RP1E4S9YUD');
